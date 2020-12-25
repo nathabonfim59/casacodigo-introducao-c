ca56de4a4d5b7d612aceb23f0d5eed02af199cc9
nathabonfim59 <dev@nathabonfim59.com>
2020-12-25 12:13:49 -0300

    Capitulo 3

---
+++ advinhacao/exercicios/1.intro.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    // Mostra a tela de boas-vindas
+    printf("***********************************\n");
+    printf("* Bem-vindo ao jovo de Advinhação *\n");
+    printf("***********************************\n");
+}
---
+++ advinhacao/exercicios/3.2.variaveis.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Mostra a tela de boas-vindas
+    printf("***********************************\n");
+    printf("* Bem-vindo ao jovo de Advinhação *\n");
+    printf("***********************************\n");
+
+    int numeroSecreto;
+    numeroSecreto = 42;
+
+    printf("O número secreto é: %d\n",  numeroSecreto);
+}
