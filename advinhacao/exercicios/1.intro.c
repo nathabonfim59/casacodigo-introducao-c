16dcd7c8056e0ffed91d834713b1928eda00e89a
nathabonfim59 <dev@nathabonfim59.com>
2020-12-25 18:23:01 -0300

    Release 1.0

--- advinhacao/exercicios/1.intro.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    // Mostra a tela de boas-vindas
-    printf("***********************************\n");
-    printf("* Bem-vindo ao jovo de Advinhação *\n");
-    printf("***********************************\n");
-}
